5fc035f821631b59530a14faf1c98f9b94327a6c
xinge <646344393@qq.com>
2019-09-02 16:42:00 +0800

    参数传递（Controller to View）1.ModelAndView

--- src/cn/qwx/controller/IndexController.java
+++ src/cn/qwx/controller/IndexController.java
@@ -5,10 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController{
     private static Logger logger=Logger.getLogger(IndexController.class);
+
+    //RequestMapping表示与那个请求的URL来对应（此处："/index"）
+    /*@RequestMapping("/index")
+    public String index(){
+        logger.info("hello,SpringMVC！");
+        return "index";
+    }*/
 
 
     @RequestMapping(value = "/welcome",method=RequestMethod.GET,params = "username")
@@ -22,15 +30,31 @@ public class IndexController{
         return "index";
     }
 
-
-
-
-    //RequestMapping表示与那个请求的URL来对应（此处："/index"）
-    @RequestMapping("/index")
-    public String index(){
-        logger.info("hello,SpringMVC！");
+    @RequestMapping("/welcome")
+    public String welcome1(@RequestParam(value ="username",required = false) String username){
+        logger.info("welcome,"+username);
         return "index";
     }
+
+
+    /**
+     * 参数传递：controller to -view -（ModelAndView）
+     * @param username
+     * @return
+     */
+    @RequestMapping("/index1")
+    public ModelAndView index(String username){
+      logger.info("welcome!username:"+username);
+      ModelAndView mView=new ModelAndView();
+      mView.addObject("username",username);
+      mView.setViewName("index");
+      return mView;
+    }
+
+
+
+
+
 
 
 }
