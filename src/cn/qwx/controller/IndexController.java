89901f6e08921ddda13bcd2177b9879cc891be09
xinge <646344393@qq.com>
2019-09-02 16:25:39 +0800

    通过请求URL进行映射，或者通过请求方法进行映射

--- src/cn/qwx/controller/IndexController.java
+++ src/cn/qwx/controller/IndexController.java
@@ -1,16 +1,36 @@
 package cn.qwx.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+@Controller
 public class IndexController{
     private static Logger logger=Logger.getLogger(IndexController.class);
-    
+
+
+    @RequestMapping(value = "/welcome",method=RequestMethod.GET,params = "username")
+
+    /**
+     *参数传递：View to Controller
+     */
+    //@RequestMapping("/welcome")
+    public String welcome(@RequestParam String username){
+        logger.info("welcome,"+username);
+        return "index";
+    }
+
+
+
+
     //RequestMapping表示与那个请求的URL来对应（此处："/index"）
     @RequestMapping("/index")
     public String index(){
         logger.info("hello,SpringMVC！");
         return "index";
     }
+
+
 }
